56d680acf7f9489e4dca0f9e0ada36a4844ae234
emrecanakisik <emrecanakisik2004@gmail.com>
2025-12-06 23:08:03 +0300

    Initial Commit

---
+++ tripscopes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tripscopes
+//
+//  Created by Emre Can Akisik on 06/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
