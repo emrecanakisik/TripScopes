e8dcfcd2dc6b1e3217661308fd7d92f6159c4bf8
emrecanakisik <emrecanakisik2004@gmail.com>
2026-04-21 17:12:36 +0300

    feat(explore): add FlightInfo and HotelInfo models

---
+++ tripscopes/Features/Explore/ViewModel/BookingCardModel.swift
@@ -0,0 +1,20 @@
+//
+//  BookingCardModel.swift
+//  tripscopes
+//
+//  Created by Emre Can Akisik on 21/04/2026.
+//
+
+import Foundation
+
+// Flight Card
+struct FlightInfo {
+    let departureCity: String
+    let arrivalCity: String
+}
+
+// Hotel Card
+struct HotelInfo {
+    let checkInDate: String
+    let guestCount: String
+}
